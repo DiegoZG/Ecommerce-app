708173723d208958ca256e16b81bf2e33d6822ec
Diwo777 <diego.zegarra79@icloud.com>
2020-11-21 19:36:11 -0500

    added hooks

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+
 
 const ProductScreen = ({ match }) => {
-    const product = products.find( p => p._id === match.params.id)
-    console.log(product)
+    const [product, setProduct] = useState({})
+
+    
+
+
     return (
         <>
          <Link className='btn btn-light my-3' to='/'>Go Back</Link>  
