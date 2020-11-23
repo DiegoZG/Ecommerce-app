383a901322fa7e9c3357fe7eedeca784e5098298
Diwo777 <diego.zegarra79@icloud.com>
2020-11-23 17:37:51 -0500

    added productReducer

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 
 dotenv.config()
 
@@ -13,15 +15,14 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('API IS RUNNING')
 })
+app.use('/api/products', productRoutes)
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use(notFound)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use(errorHandler)
+
+
+
 
 const PORT = process.env.PORT || 5000
 
