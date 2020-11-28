7757d61fb23f4ed39351ade0a8022552e0fddec1
Diwo777 <diego.zegarra79@icloud.com>
2020-11-28 14:13:14 -0500

    added placeorderscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
   return (
