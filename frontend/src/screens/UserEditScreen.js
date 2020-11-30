682282186dcdc2f95461ad8235082ffeb3a40957
Diwo777 <diego.zegarra79@icloud.com>
2020-11-30 15:12:58 -0500

    fixed bugs in useredit screen

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -1,94 +1,104 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Button, Row, Col} from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { register } from '../actions/userActions'
 import FormContainer from '../components/FormContainer'
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/userConstants'
 
-const UserEditScreen = ({ location, history }) => {
-    const [email, setEmail] = useState('')
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [message, setMessage] = useState(null)
+const UserEditScreen = ({ match, history }) => {
+  const userId = match.params.id
 
-    const dispatch = useDispatch()
-    const userRegister = useSelector(state => state.userRegister)
-    const { loading, error, userInfo } = userRegister
-    const redirect = location.search ? location.search.split('=')[1] : '/'
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [isAdmin, setIsAdmin] = useState(false)
 
-    useEffect( () => {
-        if(userInfo) {
-            history.push(redirect)
-        }
-    }, [history, userInfo, redirect])
+  const dispatch = useDispatch()
 
-    const submitHandler = (e) => {
-        e.preventDefault()
-        if(password !== confirmPassword ) {
-            setMessage('Passwords do not match')
-        } else {
-            dispatch(register(name,email,password)) 
-        }
+  const userDetails = useSelector((state) => state.userDetails)
+  const { loading, error, user } = userDetails
+
+  const userUpdate = useSelector((state) => state.userUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate
+
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET })
+      history.push('/admin/userlist')
+    } else {
+      if (!user.name || user._id !== userId) {
+        dispatch(getUserDetails(userId))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+        setIsAdmin(user.isAdmin)
+      }
     }
+  }, [dispatch, history, userId, user, successUpdate])
 
-    return (
-        <FormContainer>
-            <h1> Sign Up </h1>
-            {message && <Message variant='danger'>{message}</Message>}
-            {error && <Message variant='danger'>{error}</Message>}
-            {loading && <Loader />}
-            <Form onSubmit={submitHandler}>
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }))
+  }
+
+  return (
+    <>
+      <Link to='/admin/userlist' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
             <Form.Group controlId='name'>
-                    <Form.Label> Name </Form.Label>
-                    <Form.Control type='name' 
-                    placeholder='Enter name' 
-                    value={name}
-                    onChange={(e)=> setName(e.target.value)}
-                    ></Form.Control>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
             </Form.Group>
 
-                <Form.Group controlId='email'>
-                    <Form.Label> Email Address </Form.Label>
-                    <Form.Control type='email' 
-                    placeholder='Enter email' 
-                    value={email}
-                    onChange={(e)=> setEmail(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
+            <Form.Group controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-                <Form.Group controlId='password'>
-                    <Form.Label> Password </Form.Label>
-                    <Form.Control type='password' 
-                    placeholder='Enter password' 
-                    value={password}
-                    onChange={(e)=> setPassword(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
+            <Form.Group controlId='isadmin'>
+              <Form.Check
+                type='checkbox'
+                label='Is Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
 
-                <Form.Group controlId='confirm  password'>
-                    <Form.Label> Confirm Password </Form.Label>
-                    <Form.Control type='password' 
-                    placeholder='Confirm password' 
-                    value={confirmPassword}
-                    onChange={(e)=> setConfirmPassword(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-
-                <Button type='submit' variant='primary'>
-                    Register
-                </Button>
-            </Form>
-
-            <Row className='py-3'>
-                <Col>
-                    Have an account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}> Login </Link>
-                </Col>
-            </Row>
-        </FormContainer>
-    )
+            <Button type='submit' variant='primary'>
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  )
 }
 
 export default UserEditScreen
