c5f2bf92a6a1c6f7e4b86fe7de2e64c581ac004e
Diwo777 <diego.zegarra79@icloud.com>
2020-11-30 20:44:12 -0500

    added productlistscreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,7 +3,10 @@ import {PRODUCT_LIST_FAIL,
     PRODUCT_LIST_SUCCESS, 
     PRODUCT_DETAILS_REQUEST, 
     PRODUCT_DETAILS_SUCCESS, 
-    PRODUCT_DETAILS_FAIL} from '../constants/productConstants'
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_DELETE_REQUEST} from '../constants/productConstants'
 import axios from 'axios'
 
 
@@ -28,5 +31,24 @@ export const listProductDetails = (id) => async(dispatch) => {
          dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data})
     } catch (e) {
         dispatch( { type: PRODUCT_DETAILS_FAIL, payload: e.response && e.response.data.message ? e.response.data.message : e.message})
+    }
+}
+
+export const deleteProduct = (id) => async(dispatch, getState) => {
+    try {
+         dispatch({ type: PRODUCT_DELETE_REQUEST })
+
+         const { userLogin: { userInfo },} = getState()
+
+         const config = {
+             headers: {
+                 Authorization: `Bearer ${userInfo.token}`
+             }
+         }
+         await axios.delete(`/api/products/${id}`, config)
+
+         dispatch({ type: PRODUCT_DELETE_SUCCESS})
+    } catch (e) {
+        dispatch( { type: PRODUCT_DELETE_FAIL, payload: e.response && e.response.data.message ? e.response.data.message : e.message})
     }
 }
